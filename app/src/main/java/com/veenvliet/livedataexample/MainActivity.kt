4e2629351218053b5a571a8cdf80f0cf49a38c9b
Brad <bveenvliet@gmail.com>
2020-10-14 11:50:53 -0400

    update for android studio 4.1, also use material theme

--- app/src/main/java/com/veenvliet/livedataexample/MainActivity.kt
+++ app/src/main/java/com/veenvliet/livedataexample/MainActivity.kt
@@ -2,7 +2,6 @@ package com.veenvliet.livedataexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.veenvliet.livedataexample.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.main_activity.*
@@ -18,11 +17,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        // attach ViewModel and button click listeners
+        // attach ViewModel and observe changes to the score LiveData
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        mainViewModel.score.observe(this, Observer { newScore ->
+        mainViewModel.score.observe(this, { newScore ->
             tvScore.text = newScore.toString()
         })
+
+        // bind button click handlers
         btnPlus.setOnClickListener { mainViewModel.incScore() }
         btnMinus.setOnClickListener { mainViewModel.decScore() }
     }
